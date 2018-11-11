da6ffb421754333bf2c47f5737cffdca565a9ece
Mike Zrimsek <mikezrimsek@gmail.com>
2018-11-11 18:11:55 -0500

    displaying random quote from google sheet

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const spreadsheet = 'https://docs.google.com/spreadsheets/d/1sZuF7YsUy8rLJJcB3tOQyaZsbdUcMsF7XMnsYlidEoQ/edit#gid=0';
+
+const handleResults = function (error, _options, response) {
+  if (!error) {
+    const rawQuotes = response.rows.slice(1); // ignore header row
+    const quotes = rawQuotes.map(rawQuote => rawQuote.cellsArray[0]);
+    const quote = quotes[Math.floor(Math.random() * quotes.length)];
+    $('body').append(`<div>${quote}</div>`);
+  }
+}
+
+sheetrock({
+  url: spreadsheet,
+  query: 'select A',
+  callback: handleResults
+});
