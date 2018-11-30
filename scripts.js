5e685d66db5bdbbb6e4afe7204f09497b3f66936
Zrimsek, Mike <mike.zrimsek@rovisys.com>
2018-11-30 15:40:01 -0500

    add quote authors

--- scripts.js
+++ scripts.js
@@ -2,15 +2,27 @@ const spreadsheet = 'https://docs.google.com/spreadsheets/d/1sZuF7YsUy8rLJJcB3tO
 
 const handleResults = function (error, _options, response) {
   if (!error) {
-    const rawQuotes = response.rows.slice(1); // ignore header row
-    const quotes = rawQuotes.map(rawQuote => rawQuote.cellsArray[0]);
+    const quoteData = response.rows.slice(1); // ignore header row
+    const quotes = quoteData.map(data => {
+      const text = data.cellsArray[0];
+      const author = data.cellsArray[1] ? data.cellsArray[1] : 'Anonymous';
+
+      return {
+        text,
+        author
+      };
+    });
     const quote = quotes[Math.floor(Math.random() * quotes.length)];
-    $('body').append(`<div class="quote">${quote}</div>`);
+    $('body').append(`
+      <div class="quote">
+        ${quote.text} - ${quote.author}
+      </div>
+    `);
   }
 }
 
 sheetrock({
   url: spreadsheet,
-  query: 'select A',
+  query: 'select A, B',
   callback: handleResults
 });
